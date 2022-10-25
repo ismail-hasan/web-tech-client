5842e90eca5cc4c9df2ebbbc1b19140bf9bc1c1d
Ismail-Hasan-Bd <fnismailhasan@gmail.com>
2022-10-25 22:13:17 +0600

    favion and details page complect

--- src/Components/CourseDetails/CourseDetails.js
+++ src/Components/CourseDetails/CourseDetails.js
@@ -6,13 +6,15 @@ const CourseDetails = () => {
   const { name, img, dec } = course;
   return (
     <div>
-      <div className="w-[50%] my-[100px] mx-auto">
-        <img className="w-[60%]" src={img} alt="" />
-        <h1 className="text-4xl py-4">{name}</h1>
-        <p>{dec}</p>
-        <Link to='/course'>
-          <button className="capitalize text-white bg-orange-500 py-[6px] px-5 rounded mt-5">see more</button>
-        </Link>
+      <div className="w-[40%] my-[60px] mx-auto border-2 shadow-md border-[#e1e1e1]">
+        <img className="rounded" src={img} alt="" />
+        <div className="px-5">
+          <h1 className="text-4xl py-4">{name}</h1>
+          <p>{dec}</p>
+          <Link to="/course">
+            <button className="mb-10 capitalize text-white bg-orange-500 py-[6px] px-5 rounded mt-5">see more</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
